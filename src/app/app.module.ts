59310c70ba48e46ad717fe4c2e243bec606d8933
VICTORIA SANTOS AMARAL DE OLIVEIRA <victoria.saoliveira@CAS0474827ma-1.local>
2026-08-19 21:45:33 -0300

    criando primeiro projeto em angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { ContentComponent } from './componentes/content/content.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { LogoComponent } from './componentes/header/logo/logo.component';
+import { BuscaComponent } from './componentes/header/busca/busca.component';
+import { NavlinksComponent } from './componentes/header/navlinks/navlinks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    LogoComponent,
+    BuscaComponent,
+    NavlinksComponent
   ],
   imports: [
     BrowserModule,
